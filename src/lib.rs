0177b66b19b3b8437d58e04c197dc005df737ca0
Troels Dalsgaard Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2024-12-04 00:16:56 +0100

    Day 3 finished.
    F' regex, but i think i know them better now

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,6 @@
 use std::{fs::File, io::Read};
 pub mod day01;
 pub mod day02;
+pub mod day03;
+
 pub mod utils;
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ use utils::get_problem_input;
 
 mod day01;
 mod day02;
+mod day03;
+
 mod utils;
 fn main() -> std::io::Result<()>{
     let solution = day01::solve(&get_problem_input(Path::new("input/day01.txt"))?);
     println!("Day01 solution = {}", solution.unwrap());
     let solution = day02::solve(&get_problem_input(Path::new("input/day02.txt"))?);
     println!("Day02 solution = {}", solution.unwrap());
+    let solution = day03::solve(&get_problem_input(Path::new("input/day03.txt"))?);
+    println!("Day03 solution = {}", solution.unwrap());
     Ok(())
 }
